9633953dc9ee098f199b03d5ee30f695b818dd6f
shailar brown <shillbot@shillbot.com>
2024-11-10 15:35:23 -0600

    Commit Unit 2

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
-import { Product } from './product.model';
-import { productsArray } from './products-data';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "./product.model";
+import { ProductsService } from "./products.service";
+import { CartService } from "@core/cart.service";
 
 @Component({
-  selector: 'bot-catalog',
-  templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css'],
+	selector: "bot-catalog",
+	templateUrl: "./catalog.component.html",
+	styleUrls: ["./catalog.component.css"]
 })
-export class CatalogComponent {
-  products: Product[] = productsArray;
-  private cart: Product[] = [];
+export class CatalogComponent implements OnInit {
+	products: Product[] = [];
 
-  constructor() { }
+	constructor(private productsService: ProductsService, private cartService: CartService) {}
 
-  addToCart(product: Product) {
-    this.cart.push(product);
-  }
+	ngOnInit(): void {
+		this.products = this.productsService.getProducts();
+	}
+
+	addToCart(product: Product) {
+		this.cartService.add(product);
+	}
 }
--- src/app/shared/cart/cart.component.ts
+++ src/app/shared/cart/cart.component.ts
@@ -1,35 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../product.model';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../product.model";
+import { CartService } from "@core/cart.service";
 
 @Component({
-  selector: 'bot-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css'],
+	selector: "bot-cart",
+	templateUrl: "./cart.component.html",
+	styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
-  private cart: Product[] = [];
+	constructor(private cartService: CartService) {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() { }
+	get cartItems() {
+		return this.cartService.cart;
+	}
 
-  get cartItems() {
-    return this.cart;
-  }
+	get cartTotal() {
+		return this.cartService.cartTotal;
+	}
 
-  get cartTotal() {
-    return this.cart.reduce((prev, next) => {
-      let discount = next.discount && next.discount > 0 ? 1 - next.discount : 1;
-      return prev + next.price * discount;
-    }, 0);
-  }
+	removeFromCart(product: Product) {
+		this.cartService.remove(product);
+	}
 
-  removeFromCart(product: Product) {
-    this.cart.filter(p => p.id !== product.id);
-  }
-
-  getImageUrl(product: Product) {
-    if (!product) return '';
-    return '/assets/images/robot-parts/' + product.imageName;
-  }
+	getImageUrl(product: Product) {
+		if (!product) return "";
+		return "/assets/images/robot-parts/" + product.imageName;
+	}
 }
